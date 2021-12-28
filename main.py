a6573e79b7fc701fde856fe98425a292a4ed3404
Zireael112 <86360076+Zireael112@users.noreply.github.com>
2021-12-28 19:08:59 +0300

    Maze

---
+++ main.py
@@ -0,0 +1,64 @@
+Nowstartfirst = input('Введите координату x начала: ')
+Nowstartsecond = input('Введите координату y начала: ')
+Nowendfirst = input('Введите координату x конца: ')
+Nowendsecond = input('Введите координату y конца: ')
+a = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+start = int(Nowstartfirst), int(Nowstartsecond)
+end = int(Nowendfirst), int(Nowendsecond)
+def make_step(k):
+  for i in range(len(m)):
+    for j in range(len(m[i])):
+      if m[i][j] == k:
+        if i>0 and m[i-1][j] == 0 and a[i-1][j] == 0:
+          m[i-1][j] = k + 1
+        if j>0 and m[i][j-1] == 0 and a[i][j-1] == 0:
+          m[i][j-1] = k + 1
+        if i<len(m)-1 and m[i+1][j] == 0 and a[i+1][j] == 0:
+          m[i+1][j] = k + 1
+        if j<len(m[i])-1 and m[i][j+1] == 0 and a[i][j+1] == 0:
+           m[i][j+1] = k + 1
+m = []
+def print_m(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            print( str(m[i][j]).ljust(2),end=' ')
+        print()
+for i in range(len(a)):
+    m.append([])
+    for j in range(len(a[i])):
+        m[-1].append(0)
+i,j = start
+m[i][j] = 1
+k = 0
+while m[end[0]][end[1]] == 0:
+    k += 1
+    make_step(k)
+i, j = end
+k = m[i][j]
+while k > 1:
+  if i > 0 and m[i - 1][j] == k-1:
+    i, j = i-1, j
+    k-=1
+  elif j > 0 and m[i][j - 1] == k-1:
+    i, j = i, j-1
+    k-=1
+  elif i < len(m) - 1 and m[i + 1][j] == k-1:
+    i, j = i+1, j
+    k-=1
+  elif j < len(m[i]) - 1 and m[i][j + 1] == k-1:
+    i, j = i, j+1
+    k -= 1
+print_m(m)
+print('Самый короткий путь: ', m[int(Nowendfirst)][int(Nowendsecond)])
+
